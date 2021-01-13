2d7624f33e3c703d65843352e55b1cb61ebb069f
Zsofia Koma <komazsofi@gmail.com>
2021-01-13 11:44:29 +0100

    make las files per pulse

--- code/download_ALS.R
+++ code/download_ALS.R
@@ -1,23 +1,43 @@
+# Argumentations
+library(optparse)
+
+option_list = list(
+  make_option(c("-p", "--path"), type="character", default=NULL, 
+              help="file path where GEDI and ALS files are located", metavar="character"),
+  make_option(c("-s", "--shapefile"), type="character", default="1", 
+              help="shapefile with pulse points location", metavar="character"));  
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 # loading packages
 
 library(lidR)
 library(rgdal)
+library(rgeos)
 
 # Set output dir for downloading the files
-outdir="D:/Sync/_Amsterdam/NextJob/LiDARPos_BangorU/interview/"
+#outdir="D:/Sync/_Amsterdam/NextJob/LiDARPos_BangorU/interview/data/"
+outdir=opt$path
 setwd(outdir)
 
-#download.file(paste("https://download.pdok.nl/rws/ahn3/v1_0/laz/","C_69GN2",".LAZ",sep=''),paste("C_69GN2",".LAZ",sep=""))
-#if the file is corrupt please download it manually from https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_laz/C_69GN2.LAZ
-
-# read GEDI shp
-areaofint=readOGR(dsn="bbox_nl.shp")
+# read GEDI pulse point shp and make buffer around it
+#level1bGeo_nl_spdf=readOGR(dsn="level1b_clip_nl_shp.shp")
+level1bGeo_nl_spdf=readOGR(dsn=opt$shapefile)
 
 # extract ALS of area of interest
+# this part is computationally ineffective - for loop over pulse points
+
 ctg = catalog(outdir)
-opt_output_files(ctg) <- paste0(outdir,"bbox_nl")
 
-new_ctg = clip_roi(ctg, areaofint)
-
-
+for (i in seq(1,length(level1bGeo_nl_spdf$sht_nmb),1)){ 
+  print(i)
+  
+  if (file.exists(paste(level1bGeo_nl_spdf$sht_nmb[i],".las",sep=""))==FALSE){
+    subset = clip_circle(ctg,level1bGeo_nl_spdf@coords[i,1],level1bGeo_nl_spdf@coords[i,2],12.5)
+  }
+  
+  if (subset@header@PHB[["Number of point records"]]>0) {
+    writeLAS(subset,paste(level1bGeo_nl_spdf$sht_nmb[i],".las",sep=""))
+  }
+}
 
